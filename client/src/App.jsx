9cfb69072ebea7a67e81cb9d9e9c8cda0f003dd2
Karthikeya914 <karthikeya.chakravaram@gmail.com>
2025-10-24 20:36:11 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,16 +9,28 @@ import { AuthContext } from "./context/AuthContext";
 import "./App.css";
 
 function App() {
+  // Access the logged-in user from AuthContext
   const { user } = useContext(AuthContext);
 
   return (
     <div className="app-wrapper">
+      {/* Set up routing for the application */}
       <Router>
         <Routes>
+          {/* Home page route */}
           <Route path="/" element={<Home />} />
+          
+          {/* Registration page route */}
           <Route path="/register" element={<Register />} />
+          
+          {/* Login page route */}
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          
+          {/* Dashboard route is protected; redirect to login if user is not logged in */}
+          <Route
+            path="/dashboard"
+            element={user ? <Dashboard /> : <Navigate to="/login" />}
+          />
         </Routes>
       </Router>
     </div>
